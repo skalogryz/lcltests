027db7beb705cc9779b52765e240fb2504ad694d
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2018-12-21 11:58:47 -0500

    adding line test

--- drawlines/unit1.pas
+++ drawlines/unit1.pas
@@ -30,13 +30,37 @@ implementation
 
 procedure TForm1.FormPaint(Sender: TObject);
 begin
-
+  DrawToCanvas(Canvas);
 end;
 
 procedure TForm1.DrawToCanvas(dst: TCanvas);
+var
+  y : integer;
+  i : integer;
+const
+  w: array [0..1] of integer = (1, 10);
+  yofs: array [0..1] of integer = (10, 50);
 begin
-  dst.Pen.Color:=clBlue;
-  dst.Line(10,10,11,10);
+  for i:=low(yofs) to high(yofs) do begin
+    y:=yofs[i];
+    dst.Pen.Color:=clBlue;
+    dst.Pen.Width:=w[i];
+    dst.Pen.EndCap:=pecFlat;
+    dst.Line(10,y,11,y);
+
+    dst.Line(20,y,20,y+1);
+
+    dst.MoveTo(30, y);
+    dst.LineTo(40, y);
+
+    dst.MoveTo(50, y);
+    dst.LineTo(50, y+10);
+
+    dst.MoveTo(60, y);
+    dst.LineTo(60+10, y+10);
+  end;
+
+//  dst.Line(10,10,11,11);
 end;
 
 end.
